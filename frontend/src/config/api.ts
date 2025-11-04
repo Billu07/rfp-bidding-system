5d603383220e466d93f1c8e1288231168e86da2d
Billah07 <78539974+Billu07@users.noreply.github.com>
2025-11-05 04:10:24 +0600

    fix: update CORS for frontend 2

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,1 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API_BASE =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000" ||
+  "https://rfp-backend-pzh8xc2pn-snows-projects-59aa3c4f.vercel.app/";
